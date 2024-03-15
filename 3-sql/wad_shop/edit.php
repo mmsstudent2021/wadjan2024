bc6ecbf8b93135b19355e66802f8b2a3c7bd1be5
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-03-15 12:27:17 +0700

    update index ui

--- 3-sql/wad_shop/edit.php
+++ 3-sql/wad_shop/edit.php
@@ -40,10 +40,10 @@ if (!$conn) {
     ?>
 
     <form action="./update.php" method="post">
-        <input type="hidden" name="row_id" value="<?= $row['id'] ?>">
-        <input type="text" name="name" value="<?= $row['name'] ?>" required>
-        <input type="text" name="price" value="<?= $row['price'] ?>" required>
-        <input type="number" name="stock" value="<?= $row['stock'] ?>" required>
+        <input class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" type="hidden" name="row_id" value="<?= $row['id'] ?>">
+        <input class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" type="text" name="name" value="<?= $row['name'] ?>" required>
+        <input class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" type="text" name="price" value="<?= $row['price'] ?>" required>
+        <input class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" type="number" name="stock" value="<?= $row['stock'] ?>" required>
         <button>Add</button>
     </form>
 </body>
