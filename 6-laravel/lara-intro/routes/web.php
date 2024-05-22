61dcf52a89d408342eae34621a2f8d42823e18cf
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-05-22 18:25:45 +0700

    finish static route

--- 6-laravel/lara-intro/routes/web.php
+++ 6-laravel/lara-intro/routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return "I'm Home";
 });
+
+
+// about-us
+
+Route::get("about-us", function () {
+    return "I'm about Page";
+});
+
+// mac
+Route::get("mac", function () {
+    return "here is macbook";
+});
+
+
+// contact
